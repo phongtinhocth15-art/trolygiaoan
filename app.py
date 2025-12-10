c2df9575653bb7a89884ae36fe0b7de5fc7418d9
phongtinhocth15-art <phongtinhocth15@gmail.com>
2025-12-10 10:50:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Nguyễn Thị Mai - Trường Tiểu học 1-5 - ĐT: 0978 64 7896</div>", unsafe_allow_html=True)
