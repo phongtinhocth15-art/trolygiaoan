a5b3c9ed527abd077ff34e5f553ef076467d7180
phongtinhocth15-art <phongtinhocth15@gmail.com>
2025-12-10 10:53:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Nguyễn Thị Mai - Trường tiểu học 1-5 - ĐT: 0978647896</p>
 </div>
 """, unsafe_allow_html=True)
 
